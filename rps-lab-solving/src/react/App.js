bb9fe6a2cfc7ab9940f12d1e1198c89fef25eaa3
Jabi <michal.jabi@gmail.com>
2021-01-29 15:06:53 +0100

    rps-lab-solving in react implementation

--- rps-lab-solving/src/react/App.js
+++ rps-lab-solving/src/react/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { gameInfo, makeAMove, randomChoice, setupGameRules } from '../game'
+import rulesProvider from '../game-rules.provider'
+import TheGame from './TheGame'
 
 function App() {
+
+    const choices = setupGameRules(rulesProvider.rockPaperScissorsLizardSpockGameRules())
+
+    const handleOptionChosen = (option) => {
+      const cpuPlayerChoice = randomChoice();
+      const result = makeAMove(option, cpuPlayerChoice);
+      return gameInfo(result, cpuPlayerChoice);
+    }
+
     return (
         <main>
            <header>
                <h1>Welcome to the app</h1>
            </header>
            <div>
-               ... here i need a game...
-           </div> 
+               <TheGame choices={choices} onOptionChosen={handleOptionChosen} />
+           </div>
         </main>
     )
 }
