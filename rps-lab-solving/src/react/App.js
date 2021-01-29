60c11b921294d60ddc9c7a983c5fa8354cc80531
Jabi <michal.jabi@gmail.com>
2021-01-29 10:16:53 +0100

    rps-lab-solving: React added to the project

---
+++ rps-lab-solving/src/react/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+    return (
+        <main>
+           <header>
+               <h1>Welcome to the app</h1>
+           </header>
+           <div>
+               ... here i need a game...
+           </div> 
+        </main>
+    )
+}
+
+export default App;
